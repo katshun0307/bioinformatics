f377d7fe054e9f2311fcf652f677ea4e229572fe
Shuntaro Katsuda <shunten3@gmail.com>
2018-06-27 16:47:03 +0900

    added file input
    run with `./a.out test.txt test2.txt`

--- main.ml
+++ main.ml
@@ -3,10 +3,8 @@ type vertex = int * int
 type res = Result of string * string * int
 
 let pp_result = function
-  | Result(s1, s2, i) -> print_string("(" ^ s1 ^ ", " ^ s2 ^ ", " ^ string_of_int i ^ ")")
+  | Result(s1, s2, i) -> print_string("(" ^ s1 ^ ", " ^ s2 ^ ", " ^ string_of_int i ^ ")\n")
 
-let seq0 = "abcdefg"
-let seq1 = "fabcdef"
 let null_char = "-"
 let reward_score = -1
 let default_score = 1
@@ -37,30 +35,37 @@ let get_min_res lst =
   let r = Result("", "", 1000) in
   loop lst r
 
-let rec distance src target = 
-  match src, target with
-  | (x, y), (w, z) -> 
-    let (a, b) as diff = (w - x, z - y) in
-    let c0 = Char.escaped seq0.[x] in
-    let c1 = Char.escaped seq1.[y] in
-    match diff with
-    | (1, 1) -> if c0 = c1 then Result(c0, c1, reward_score) else Result(c0, c1, default_score)
-    | (0, 1) -> Result(null_char, c1, default_score)
-    | (1, 0) -> Result(c0, null_char, default_score)
-    | (0, 0) -> Result("", "", 0)
-    | _ -> 
-      (* middles *)
-      let parents = get_parent target in
-      (* src -> middle *)
-      let induction_distances = List.map (distance src) parents in
-      (* middle -> target *)
-      let hop_1_distances = List.map (flip distance target) parents in
-      let all_paths = List.map2 combine induction_distances hop_1_distances in
-      let min = get_min_res all_paths in min
+let allignment s0 s1 = 
+  let seq0 = s0 in
+  let seq1 = s1 in
+  let rec distance src target = 
+    match src, target with
+    | (x, y), (w, z) -> 
+      let (a, b) as diff = (w - x, z - y) in
+      let c0 = Char.escaped seq0.[x] in
+      let c1 = Char.escaped seq1.[y] in
+      match diff with
+      | (1, 1) -> if c0 = c1 then Result(c0, c1, reward_score) else Result(c0, c1, default_score)
+      | (0, 1) -> Result(null_char, c1, default_score)
+      | (1, 0) -> Result(c0, null_char, default_score)
+      | (0, 0) -> Result("", "", 0)
+      | _ -> 
+        (* middles *)
+        let parents = get_parent target in
+        (* src -> middle *)
+        let induction_distances = List.map (distance src) parents in
+        (* middle -> target *)
+        let hop_1_distances = List.map (flip distance target) parents in
+        let all_paths = List.map2 combine induction_distances hop_1_distances in
+        let min = get_min_res all_paths in min in
+  distance (0, 0) (String.length seq0 -1, String.length seq1 -1)
 
-let haha = distance (0, 0) (6, 6);;
-pp_result haha;;
-
-let test_res_list = Result("", "", 1) :: Result("", "", 5) :: []
-
-let hoge = get_min_res test_res_list;;
+let main = 
+  if Array.length Sys.argv = 3 then
+    let in_ch1 = open_in Sys.argv.(1) in
+    let in_ch2 = open_in Sys.argv.(2) in
+    let seq0 = input_line in_ch1 in
+    let seq1 = input_line in_ch2 in
+    let haha = allignment seq0 seq1 in
+    pp_result haha
+  else print_string "put in two file names\n"
